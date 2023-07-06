772b98f30f9ed1b768c1eb492718008fdaa74538
Jacqueline Watson-Sinclair <faycello75@gmail.com>
2023-07-05 23:17:39 -0400

    defined function expression and arrow function and gained clarity regarding explicit vs. implicit use of the return keyword

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a, b) {
+    return a/b; 
+   }
+  console.log(divide(2000, 100));
+
+const square = x => x ** 2;
+console.log(square(2));
+
+const add = (a, b) => a + b;
+console.log(add(3, 4));
